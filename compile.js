877e9c239e193ecc30ad2f07d0d1156e6079da01
Matthew Francis-Landau <matthew@matthewfl.com>
2009-07-24 00:54:26 -0700

    removeing old code

--- compile.js
+++
@@ -1,7 +0,0 @@
-// compile
-;(function (Py) {
-		Py.compile = function (code) {
-				code = code.replace("\/\/[^\n]+", "");
-				Py.debug(code);
-		};
-})(Python['.']);
--- interface.js
+++
@@ -1,6 +0,0 @@
-// interface
-;(function (Py, _Py) {
-		Py.run = function (code) {
-				_Py.compile(code);
-		};
-})(Python, Python['.']);
